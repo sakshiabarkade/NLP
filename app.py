e9dfa9d610754017b36f7cce1258ce3ee2018ee4
sakshiabarkade <barkadesakshi50@gmail.com>
2026-03-16 15:00:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5fb65e6c-63b3-4eb7-8091-3e166e99067c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
